f468a248eb98ab2e1d4a9b9e366e92b75d001fbf
Michael Hughes <michael@mihughes.com>
2017-04-12 21:42:46 -0600

    Add module describing georef precisions

---
+++ src/GeoRefPrecision.js
@@ -0,0 +1,30 @@
+/**
+ Defines friendly names for different levels of Georef precision
+ @module
+ */
+
+/**
+ * The size/type of a Georef quadrangle
+ * @typedef {number} QuadSize
+ */
+
+/**
+ * @type {QuadSize}
+ */
+export const FifteenDegreeQuad = 2;
+/**
+ * @type {QuadSize}
+ */
+export const OneDegreeQuad = 4;
+/**
+ * @type {QuadSize}
+ */
+export const OneArcMinuteQuad = 8;
+/**
+ * @type {QuadSize}
+ */
+export const OneTenthArcMinuteQuad = 10;
+/**
+ * @type {QuadSize}
+ */
+export const OneHundrethArcMinuteQuad = 12;
