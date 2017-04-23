0a6128eedef6bf8f9a6a4377f5409902eda3759d
Michael Hughes <michael@mihughes.com>
2017-04-23 10:16:23 -0600

    Add initial set of tests for Maidenhead Locator implementation

--- src/MaidenheadLocator.test.js
+++ src/MaidenheadLocator.test.js
@@ -15,3 +15,32 @@
  */
 
 import { mhLocatorFromLatLng, latLngFromMhLocator } from './MaidenheadLocator';
+
+describe('Convert from latitude and longitude to Maidenhead Locator Square', () => {
+  test('correctly handles top level locator square of 0, 0', () => {
+    expect(mhLocatorFromLatLng(0, 0)).toBe('JJ');
+  });
+
+  test('correctly handles top level locator square of Newington Connecticut to 4 characters', () => {
+    expect(mhLocatorFromLatLng(41.71463, -72.72713)).toBe('FN31pr');
+  });
+
+  test('correctly handles the extended square of Paris, France', () => {
+    //TODO: Calculate extended square and add to test
+    expect(mhLocatorFromLatLng(48.85207, 2.3909)).toBe('JN18EU');
+  });
+});
+
+describe('Convert from Maidenhead Locator Square to averaged latitude longitude', () => {
+  test('correctly averages location of AA', () => {
+
+  });
+
+  test('correctly averages location of XX', () => {
+
+  });
+
+  test('correctly averages location of FN31pr', () => {
+
+  });
+});
