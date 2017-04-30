dd5bdb39ed070a177d3d85df76354a249ba53dde
Michael Hughes <michael@mihughes.com>
2017-04-30 16:38:37 -0600

    Add additional bounds checks, start implementing reverse lookup for Maidenhead locator

--- src/MaidenheadLocator.js
+++ src/MaidenheadLocator.js
@@ -15,10 +15,13 @@
  */
 
 import { boundCheckAndAdd, boundCheckAndRetrieve } from './Common';
+import { round10 } from './DecimalRounding';
 
 /**
- * Implements functions for converting to/from a Maidenhead Locator using WGS84 latitude and longitude
- * coordinates.
+ * Implements functions for converting to/from a Maidenhead Locator using WGS84 latitude and
+ * longitude
+ * coordinates. Please note that implemented library functions only implement subsquare precision,
+ * that is up to 6 characters are handled.
  *
  * Original description of system by @see {@link http://www.arrl.org/grid-squares|ARRL}
  * @module
@@ -27,7 +30,6 @@ import { boundCheckAndAdd, boundCheckAndRetrieve } from './Common';
 const firstLevelDivisions = 18;
 const firstLevelLongitudeDivision = 360 / firstLevelDivisions;
 const firstLevelLatitudeDivision = 180 / firstLevelDivisions;
-const secondLevelLongitudeDivisions = 2;
 const thirdLevelDivisions = 24;
 const degreeArcMinutes = 60;
 const thirdLevelLongitudeDivisions = (2 * degreeArcMinutes) / thirdLevelDivisions;
@@ -59,14 +61,14 @@ const alphabet = [
   'u',
   'v',
   'w',
-  'x'
+  'x',
 ];
 
 const charMap = alphabet.reduce((accumulator, val, idx) => {
   /* eslint no-param-reassign: 0 */
-  accumulator[idx] = val;
+  accumulator[val] = idx;
   return accumulator;
-},{});
+}, {});
 
 
 /**
@@ -76,6 +78,9 @@ const charMap = alphabet.reduce((accumulator, val, idx) => {
  * @return {string} A Maidenhead Locator Square
  */
 export function mhLocatorFromLatLng(latitude, longitude) {
+  if (latitude < -90 || latitude > 180 || longitude < -180 || longitude > 180) {
+    throw new Error(`${latitude},${longitude} was not a valid decimal WGS84 pair`);
+  }
   const characters = [];
 
   // In maidenhead locators are calculated as easting and northing from -90 S and -180 W
@@ -117,5 +122,65 @@ export function mhLocatorFromLatLng(latitude, longitude) {
  * @return {object} A point object with latitude and longitude
  */
 export function latLngFromMhLocator(mhLocator) {
+  if (!mhLocator || mhLocator.length % 2 !== 0 || mhLocator.length < 2 || mhLocator.length > 6) {
+    throw new Error(`Locator, ${mhLocator}, is badly formed by length`);
+  }
 
+  return getLatLngFromMhLocator(mhLocator.split(''), true);
+}
+
+function getLatLngFromMhLocator(locatorChars, last = false) {
+  switch (locatorChars.length) {
+    case 2: {
+      const point = {
+        latitude: startingLatitude,
+        longitude: startingLongitude,
+      };
+      let idx = boundCheckAndRetrieve(locatorChars[0].toLowerCase(), charMap);
+      point.longitude += idx * firstLevelLongitudeDivision;
+      idx = boundCheckAndRetrieve(locatorChars[1].toLowerCase(), charMap);
+      point.latitude += idx * firstLevelLatitudeDivision;
+      if (last) {
+        point.longitude = round10((firstLevelLongitudeDivision / 2) + point.longitude, 0);
+        point.latitude = round10((firstLevelLatitudeDivision / 2) + point.latitude, 0);
+      }
+      return point;
+    }
+    case 4: {
+      const point = getLatLngFromMhLocator(locatorChars.slice(0, 2));
+      let lng;
+      let lat;
+      try {
+        lng = parseInt(locatorChars[2], 10) * 2;
+        lat = parseInt(locatorChars[3], 10);
+      } catch (e) {
+        throw new Error(`Invalid characters found in locator, was either ${locatorChars[2]} or
+        ${locatorChars[3]}`);
+      }
+      point.longitude += lng;
+      point.latitude += lat;
+      if (last) {
+        point.longitude = round10((point.longitude + 1), -1);
+        point.latitude = round10((point.latitude + 0.5), -1);
+      }
+      return point;
+    }
+    case 6: {
+      const point = getLatLngFromMhLocator(locatorChars.slice(0, 4));
+      const LngIdx = boundCheckAndRetrieve(locatorChars[4].toLowerCase(), charMap);
+      const LatIdx = boundCheckAndRetrieve(locatorChars[5].toLowerCase(), charMap);
+      point.longitude += ((LngIdx * thirdLevelLongitudeDivisions) + last ? 2.5 : 0)
+        / degreeArcMinutes;
+      point.latitude += ((LatIdx * thirdLevelLatitudeDivisions) + last ? 1.25 : 0)
+        / degreeArcMinutes;
+      if (last) {
+        point.longitude = round10(point.longitude, -1);
+        point.latitude = round10(point.latitude, -1);
+      }
+      return point;
+    }
+    default: {
+      throw new Error(`Invalid number of Maidenhead Locator characters ${locatorChars.length}`);
+    }
+  }
 }
